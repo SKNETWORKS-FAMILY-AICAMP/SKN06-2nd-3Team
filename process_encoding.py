1b075dbf7db2e9f9a14f83ab10e1eded21d2881b
suyeon <en123460@naver.com>
2024-11-16 15:32:20 +0900

    process_encoding.py streamlit.py update

--- process_encoding.py
+++ process_encoding.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 
 def dropcol(df):
@@ -16,34 +15,41 @@ def dropcol(df):
     df.dropna(axis=0, inplace=True)
     return df
 
-def convertint(X):
-    X.Churn = X.Churn.replace("Yes", 1)
-    X.Churn = X.Churn.replace("No", 0)
+def convertint(X):   
+    try:
+        X.loc[X['Churn'] == 'Yes', 'Churn'] = 1
+        X.loc[X['Churn'] == 'No', 'Churn'] = 0
+    except:
+        print("변환 안됨")
     
     region_dict = {
     'East': ['BOS', 'PHI', 'NYC', 'PIT', 'MIA', 'ATL', 'HAR', 'NSH', 'NCR', 'NNY'],
     'Center': ['CHI', 'DET', 'STL', 'DAL', 'HOU', 'KCY', 'OMA', 'IND', 'INH', 'IPM', 'AWI', 'FLN', 'OHI', 'OHH'],
     'West': ['LAX', 'SFR', 'SEA', 'SAN', 'PHX', 'DEN', 'SLC', 'LAU', 'NEV', 'NMC', 'NMX', 'NVU', 'HWI', 'SHE', 'SDA', 'SEW', 'SFU', 'SLU']
     }
-
     def map_region(service_area):
-        txt = service_area[:3]
         for region, cities in region_dict.items():
-            if txt in [city[:3] for city in cities]:
+            if service_area[:3] in [city[:3] for city in cities]:
                 return region
             
     X.ServiceArea = X.ServiceArea.apply(map_region)
-            
     try:
-        X.CreditRating = X.CreditRating.replace("1-Highest", 1)
-        X.CreditRating = X.CreditRating.replace("2-High", 2)
-        X.CreditRating = X.CreditRating.replace("3-Good", 3)
-        X.CreditRating = X.CreditRating.replace("4-Medium", 4)
-        X.CreditRating = X.CreditRating.replace("5-Low", 5)
-        X.CreditRating = X.CreditRating.replace("6-VeryLow", 6)
-        X.CreditRating = X.CreditRating.replace("7-Lowest", 7)
+        credit_rating_map = {
+            "1-Highest": 1,
+            "2-High": 2,
+            "3-Good": 3,
+            "4-Medium": 4,
+            "5-Low": 5,
+            "6-VeryLow": 6,
+            "7-Lowest": 7
+        }
+        
+        for key, val in credit_rating_map.items():
+            X.loc[X['CreditRating'] == key, 'CreditRating'] = val
+        X['CreditRating'] = X['CreditRating'].astype('float64')
     except:
         print("변환 안됨")
+    return X
         
 def ifmode(df, catecol):
     threshold = 0.05 
@@ -53,16 +59,15 @@ def ifmode(df, catecol):
         min_percentage = value_counts.min()
         if min_percentage < threshold:
             col_replace.append(col)
-            print(col_replace)
         
         for column in col_replace:
             mode_value = df[column].mode()[0] 
             value_counts = df[column].value_counts(normalize=True)
             rare_values = value_counts[value_counts < 0.05].index
-            df[column] = df[column].replace(rare_values, mode_value)
+            df.loc[df[column].isin(rare_values), column] = mode_value
     return df
-    
-# 원핫인코딩
+
+# 원핫인코딩    
 def ohot(df, column):
     threshold = 10  
     ohecol = []
@@ -70,14 +75,8 @@ def ohot(df, column):
         unique_count = df[col].nunique()
         if unique_count <= threshold:
             ohecol.append(col)
-    df = pd.get_dummies(df, columns=ohecol)
-    # ohe = OneHotEncoder()
-    # ohe_array = ohe.fit_transform(df[ohecol]).toarray()
-    # ohe_df = pd.DataFrame(ohe_array, columns=ohe.get_feature_names_out(ohecol), index=df.index)
-    # df = pd.concat([df.drop(columns=ohecol), ohe_df], axis=1)
+    return pd.get_dummies(df, columns=ohecol)
     
-    return df
-
 # 이상치처리_IQR
 def outlier_iqr(df, whis=2.0):
     median_group = []
@@ -85,7 +84,6 @@ def outlier_iqr(df, whis=2.0):
     q1_q3_group = []
     upper_bound_group = []
     lower_bound_group = []
-
     for i in df.columns:
         X = df[i]
         q1 = np.nanquantile(X, q=0.25)
@@ -93,7 +91,6 @@ def outlier_iqr(df, whis=2.0):
         IQR = q3 - q1
         lower_bound = q1 - whis * IQR
         upper_bound = q3 + whis * IQR
-        
         if X.skew() > 1 or X.skew() < -1:
             median_group.append(i)
         elif X.between(lower_bound, upper_bound).mean() > 0.95:
@@ -114,7 +111,6 @@ def outlier_iqr(df, whis=2.0):
         upper_bound = q3 + 1.5 * IQR
         df.loc[(df[column] < lower_bound) | (df[column] > upper_bound), column] = median
 
-
     for column in iqr_group:
         q1 = df[column].quantile(0.25)
         q3 = df[column].quantile(0.75)
@@ -130,14 +126,11 @@ def outlier_iqr(df, whis=2.0):
         upper_bound = q3 + 1.5 * IQR
         df.loc[df[column] > upper_bound, column] = q3
 
-
-
     for column in lower_bound_group:
         q1 = df[column].quantile(0.25)
         IQR = df[column].quantile(0.75) - df[column].quantile(0.25)
         lower_bound = q1 - 1.5 * IQR
         df.loc[df[column] < lower_bound, column] = q1
-
     return df
     
 
@@ -148,22 +141,23 @@ def load_df(return_X_y=False, validset=True, test_size = 0.2, random_state=42):
     traindf = dropcol(traindf)
     
     # 정수 변환
-    convertint(traindf)
+    traindf = convertint(traindf)
     
     # 이상치 처리
     num_col = traindf.select_dtypes(include=['int64', 'float64']).columns
-    cate_col = traindf.select_dtypes(include=['object', 'category']).columns
+    cate_col = traindf.select_dtypes(include=['object']).columns
     cate_col = [i for i in cate_col if i not in "Churn"]
     traindf[num_col] = outlier_iqr(traindf[num_col])
     traindf[cate_col] = ifmode(traindf[cate_col], cate_col)
     
     # 인코딩
-    traindf = ohot(traindf, cate_col)
+    traindf_ohe = ohot(traindf, cate_col)
+    traindf = pd.concat([traindf.drop(columns=cate_col), traindf_ohe], axis=1)
+    traindf = traindf.loc[:, ~traindf.columns.duplicated()]
+    y = traindf.Churn
+    X = traindf.drop(columns="Churn")
     
     if return_X_y:
-        y = traindf.Churn
-        X = traindf.drop(columns="Churn")
-
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
         if validset:
             X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=test_size, random_state=random_state)
@@ -171,14 +165,16 @@ def load_df(return_X_y=False, validset=True, test_size = 0.2, random_state=42):
         return X_train, X_test, y_train, y_test
     return traindf
 
-def processor(inputdata):
-    num_col = inputdata.select_dtypes(include=[np.number]).columns
-    cate_col = inputdata.select_dtypes(include=['object', 'category']).columns
-    inputdata[num_col] = outlier_iqr(inputdata[num_col])
-    inputdata[cate_col] = ifmode(inputdata[cate_col], cate_col)
-    inputdata = ohot(inputdata, cate_col)
-    return inputdata
+def processor(input_data):
+    input_data = convertint(input_data)
+    num_col = input_data.select_dtypes(include=[np.number]).columns
+    cate_col = input_data.select_dtypes(include=['object', 'category']).columns
+    input_data[num_col] = outlier_iqr(input_data[num_col])
+    input_data_ohe = ohot(input_data, cate_col)
+    input_data = pd.concat([input_data.drop(columns=cate_col), input_data_ohe], axis=1)
+    input_data = input_data.loc[:, ~input_data.columns.duplicated()]
+    return input_data
+
 
 if __name__ == "__main__":
-    load_df().to_csv('./chk.csv', index=False)
-    # print(load_df().columns)
+    load_df()
---
+++ streamlit.py
@@ -0,0 +1,120 @@
+# streamlit run streamlit.py
+import streamlit as st
+import process_encoding as pe
+import joblib
+import numpy as np
+import pandas as pd
+import sys
+import os
+
+# Streamlit 앱 설정
+st.set_page_config(layout="wide", page_title="고객 이탈률 예측", page_icon="🔍")
+st.title('고객 이탈률 예측')
+
+@st.cache_resource
+def load_model():
+    return joblib.load('models/best_xgb.pkl')
+
+# 모델 및 전처리기 로드
+model = load_model()
+
+# 입력
+st.sidebar.header('입력 정보')
+MonthlyRevenue = st.sidebar.number_input('월별 수익', min_value=-999.0, value=0.0)
+MonthlyMinutes = st.sidebar.number_input('월별 통화 분수', min_value=0.0, value=0.0)
+TotalRecurringCharge = st.sidebar.number_input('총 정기 요금', min_value=-999.0, value=0.0)
+DirectorAssistedCalls = st.sidebar.number_input('상담사 도움을 받은 건수', min_value=0.0, value=0.0)
+OverageMinutes = st.sidebar.number_input('초과 통화 분수', min_value=0.0, value=0.0)
+RoamingCalls = st.sidebar.number_input('로밍 통화 수', min_value=0.0, value=0.0)
+PercChangeMinutes = st.sidebar.number_input('통화 분수의 변동률', min_value=-999.0, value=0.0)
+PercChangeRevenues = st.sidebar.number_input('수익의 변동률', min_value=-999.0, value=0.0)
+DroppedCalls = st.sidebar.number_input('통화 중 끊어진 통화 수', min_value=0.0, value=0.0)
+BlockedCalls = st.sidebar.number_input('차단된 통화 수', min_value=0.0, value=0.0)
+UnansweredCalls = st.sidebar.number_input('응답받지 못한 통화 수', min_value=0.0, value=0.0)
+CustomerCareCalls = st.sidebar.number_input(' 고객 센터 통화 수', min_value=0.0, value=0.0)
+ReceivedCalls = st.sidebar.number_input('받은 통화 수', min_value=0.0, value=0.0)
+OutboundCalls = st.sidebar.number_input('발신 통화 수', min_value=0.0, value=0.0)
+InboundCalls = st.sidebar.number_input('수신 통화 수', min_value=0.0, value=0.0)
+PeakCallsInOut = st.sidebar.number_input('피크 시간대의 발신 및 수신 통화 수', min_value=0.0, value=0.0)
+DroppedBlockedCalls = st.sidebar.number_input('끊어진 및 차단된 통화 수', min_value=0.0, value=0.0)
+CallWaitingCalls = st.sidebar.number_input('통화 대기 중인 통화 수', min_value=0.0, value=0.0)
+MonthsInService  = st.sidebar.number_input('서비스 가입 월수', min_value=0, value=0)
+UniqueSubs  = st.sidebar.number_input('고유 가입자 수', min_value=0, value=0)
+ActiveSubs  = st.sidebar.number_input('활성 가입자 수', min_value=0, value=0)
+Handsets = st.sidebar.number_input('휴대폰 수', min_value=0, value=0)
+CurrentEquipmentDays = st.sidebar.number_input('현재 장비 사용 일수', min_value=0.0, value=0.0)
+HandsetModels = st.sidebar.number_input('휴대폰 모델 수', min_value=0, value=0)
+IncomeGroup = st.sidebar.number_input('소득 그룹', min_value=0, max_value=9, value=0)
+
+# 범주형
+children_in_HH = ["Yes", "No"]
+service_area_options = [
+    'BOS', 'PHI', 'NYC', 'PIT', 'MIA', 'ATL', 'HAR', 'NSH', 'NCR', 'NNY',
+    'CHI', 'DET', 'STL', 'DAL', 'HOU', 'KCY', 'OMA', 'IND', 'INH', 'IPM', 'AWI', 'FLN', 'OHI', 'OHH',
+    'LAX', 'SFR', 'SEA', 'SAN', 'PHX', 'DEN', 'SLC', 'LAU', 'NEV', 'NMC', 'NMX', 'NVU', 'HWI', 'SHE', 'SDA', 'SEW', 'SFU', 'SLU'
+]
+handset_web_capable_options = ["Yes", "No"]
+has_credit_card_options = ["Yes", "No"]
+new_cellphone_user_options = ["Yes", "No"]
+credit_rating_options = ["1-Highest", "2-High", "3-Good", "4-Medium", "5-Low", "6-VeryLow", "7-Lowest"]
+prizm_code_options = ["Town", "Suburban", "Rural", "Other"]
+occupation_options = ["Professional", "Crafts", "Clerical", "Self", "Retired", "Student", "Homemaker", "Other"]
+
+# 컬럼별 선택 위젯
+ChildrenInHH = st.sidebar.selectbox('가구 내 자녀 수:', children_in_HH)
+service_area = st.sidebar.selectbox("서비스 지역:", service_area_options)
+handset_web_capable = st.sidebar.selectbox("웹 사용 가능한 휴대폰 여부:", handset_web_capable_options)
+has_credit_card = st.sidebar.selectbox("신용카드 보유 여부:", has_credit_card_options)
+new_cellphone_user = st.sidebar.selectbox("신규 휴대폰 사용자 여부:", new_cellphone_user_options)
+credit_rating = st.sidebar.selectbox("신용 등급:", credit_rating_options)
+prizm_code = st.sidebar.selectbox("프리즘 코드:", prizm_code_options)
+occupation = st.sidebar.selectbox("직업:", occupation_options)
+# 예측 버튼
+btn = st.sidebar.button('예측')
+if btn:
+    input_data = pd.DataFrame({
+        'MonthlyRevenue': [MonthlyRevenue],
+        'MonthlyMinutes': [MonthlyMinutes],
+        'TotalRecurringCharge': [TotalRecurringCharge],
+        'DirectorAssistedCalls': [DirectorAssistedCalls],
+        'OverageMinutes': [OverageMinutes],
+        'RoamingCalls': [RoamingCalls],
+        'PercChangeMinutes': [PercChangeMinutes],
+        'PercChangeRevenues': [PercChangeRevenues],
+        'DroppedCalls': [DroppedCalls],
+        'BlockedCalls': [BlockedCalls],
+        'UnansweredCalls': [UnansweredCalls],
+        'CustomerCareCalls': [CustomerCareCalls],
+        'ReceivedCalls': [ReceivedCalls],
+        'OutboundCalls': [OutboundCalls],
+        'InboundCalls': [InboundCalls],
+        'PeakCallsInOut': [PeakCallsInOut],
+        'DroppedBlockedCalls': [DroppedBlockedCalls],
+        'CallWaitingCalls': [CallWaitingCalls],
+        'MonthsInService': [MonthsInService],
+        'UniqueSubs': [UniqueSubs],
+        'ActiveSubs': [ActiveSubs],
+        'Handsets': [Handsets],
+        'CurrentEquipmentDays': [CurrentEquipmentDays],
+        'ChildrenInHH': [ChildrenInHH],
+        'HandsetModels': [HandsetModels],
+        'ServiceArea': [service_area],
+        'HandsetWebCapable': [handset_web_capable],
+        'HasCreditCard': [has_credit_card],
+        'NewCellphoneUser': [new_cellphone_user],
+        'IncomeGroup': [IncomeGroup],
+        'CreditRating': [credit_rating],
+        'PrizmCode': [prizm_code],
+        'Occupation': [occupation],
+    })
+    st.table(input_data)
+    preprocessor = pe.processor(input_data)
+    
+    # # 예측 수행
+    # prediction = model.predict(preprocessor.to_numpy())
+    # prediction_proba = model.predict_proba(preprocessor)[:, 1]
+
+    # # 결과 출력
+    # st.header('예측 결과')
+    # st.subheader(f'이탈여부 예측: {"이탈" if prediction[0] == 1 else "이탈X"}')
+    # st.write(f'이탈 확률: {prediction_proba[0]*100:.2f}%')
